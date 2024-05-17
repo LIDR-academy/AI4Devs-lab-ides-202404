a74784c38cfb9fa07631e31470295971e9596464
Daniel <oscar.d.gonzalez@gmail.com>
2024-05-16 22:59:32 -0500

    updated css styling

--- frontend/src/components/Dashboard/index.tsx
+++ frontend/src/components/Dashboard/index.tsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 
 export const Dashboard: React.FC = () => {
   return (
-    <div>
-      <h1>Recruiter Dashboard</h1>
+    <div className='flex flex-col items-center'>
+      <h1 className="text-2xl sm:text-3xl lg:text-4xl mb-4">Dashboard del reclutador</h1>
       <Link to="/add-candidate" className="bg-blue-500 text-white font-bold py-2 px-4 rounded mt-4">Añadir Nuevo Candidato</Link>
     </div>
   );
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -3,8 +3,10 @@ import { Dashboard } from '../../components/Dashboard';
 
 export const Home: React.FC = () => {
   return (
-    <div>
-      <Dashboard />
+    <div className="flex flex-col items-center justify-center min-h-screen px-4 sm:px-6 lg:px-8">
+      <div className="w-full max-w-md">
+        <Dashboard />
+      </div>
     </div>
   );
 };
