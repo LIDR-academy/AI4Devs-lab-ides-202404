a73465347446fc75ac3b757dbdf856fba90b8d31
Daniel <oscar.d.gonzalez@gmail.com>
2024-05-16 21:58:33 -0500

    servers limit increase

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
   res.send('Hola LTI!');
 });
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(cors());
 app.use('/api', candidateRoutes);
 app.use(errorHandlerMiddleware);
