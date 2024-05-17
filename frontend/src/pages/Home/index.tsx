baad1665078cc6b048df39e35cfdbc570fe93512
Daniel <oscar.d.gonzalez@gmail.com>
2024-05-16 22:03:55 -0500

    Home component

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Dashboard } from '../../components/Dashboard';
+
+export const Home: React.FC = () => {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+};
